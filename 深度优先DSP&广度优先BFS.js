04e23f0102462782985ec22c62f5b89739c88a8d
weiyb <weiyb@jiedaibao.com>
2020-06-23 15:05:12 +0800

    深度优先和广度优先

--- 深度优先DSP&广度优先BFS.js
+++ 深度优先DSP&广度优先BFS.js
@@ -1,1 +1,13 @@
-// 深度优先是指
+const tree = require('./tree.js')
+
+// 深度优先是指对每一个分支路径深入到不能再深入为止，并且每个节点只访问一次
+
+function dfs(data) {
+  data.forEach(item => {
+    console.log(item.name)
+    dfs(item.children)
+  })
+}
+
+// todo 看完二叉树算法再补齐
+dfs(tree)
