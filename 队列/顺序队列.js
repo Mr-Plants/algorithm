4b7330b51d39969e8d87491da224ac10666dda02
Mr-Plants <wyb19931126/>
2021-01-22 00:09:49 +0800

    队列

--- 队列/顺序队列.js
+++ 队列/顺序队列.js
@@ -22,6 +22,7 @@ class SequenceQueue {
     // enqueue时如果队列满了，需要将head->tail直接的数据搬运到起始位置
     if (this.tail === this.length) {
       if (this.head === 0) return false;
+      console.log('开始搬运')
       for (let i = this.head; i < this.tail; i++) {
         this.items[i - this.head] = this.items[i]
       }
@@ -44,19 +45,21 @@ class SequenceQueue {
   }
 }
 
-//todo  测试代码有问题！
-
 // test
 let queue = new SequenceQueue(5)
 for (let i = 0; i < 100; i++) {
   console.log(queue.head, queue.tail)
-  console.log(queue.items)
-  if (Math.random() < 0.5) {
-    queue.enqueue('ran' + parseInt(Math.random() * 100))
-  } else {
-    queue.dequeue();
-  }
 
+  if (Math.random() < 0.5) {
+    const val = 'ran' + parseInt(Math.random() * 100);
+    queue.enqueue(val)
+    console.log('入栈：', val)
+  } else {
+
+    const out = queue.dequeue();
+    console.log('出栈：', out)
+  }
+  console.log(queue.items)
 }
 
 
