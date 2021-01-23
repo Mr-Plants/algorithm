c4348f81ec7eccfa3970480a325ee68ac2df9bf8
weiyb <weiyb@jiedaibao.com>
2021-01-23 13:37:46 +0800

    递归

---
+++ 递归/找到推荐人.js
@@ -0,0 +1,45 @@
+const data = [
+  {
+    actor_Id: 'A',
+    referrer_id: null
+  },
+  {
+    actor_Id: 'B',
+    referrer_id: 'A'
+  },
+  {
+    actor_Id: 'C',
+    referrer_id: 'B'
+  },
+  {
+    actor_Id: 'D',
+    referrer_id: ''
+  },
+  {
+    actor_Id: 'E',
+    referrer_id: 'D'
+  },
+  {
+    actor_Id: 'F',
+    referrer_id: 'E'
+  },
+]
+
+function findRootReferrer(actorID) {
+  const actor = data.find(i => i.actor_Id === actorID);
+  if (!actor.referrer_id) return actorID;
+  // 找到推荐人
+  return findRootReferrer(actor.referrer_id);
+}
+
+
+// test
+console.log(findRootReferrer('C'))
+console.log(findRootReferrer('D'))
+console.log(findRootReferrer('E'))
+
+
+/*递推公式
+* 首先缩小问题规模：
+* 要找到我的最终推荐人，需要先找到我的推荐人是谁，再找推荐人的推荐人
+* 递归结束条件就是找不到新的推荐人*/
