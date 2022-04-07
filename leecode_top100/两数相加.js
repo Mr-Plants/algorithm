abca904ae07f3ce8606f6686f84b6ca693c98758
weiyabo1 <weiyabo1@jd.com>
2022-04-07 20:56:19 +0800

    top100

--- leecode_top100/两数相加.js
+++ leecode_top100/两数相加.js
@@ -38,9 +38,8 @@ a.append(3)
 a.append(5)
 
 let b = new LinkedNodeList();
-b.append(2)
-// b.append(4)
-// b.append(6)
+b.append(6)
+b.append(9)
 
 function reverseLinkedNodeList2String(head) {
     let arr = [];
@@ -53,17 +52,55 @@ function reverseLinkedNodeList2String(head) {
     return arr.reverse().join('');
 }
 
-let numA = +reverseLinkedNodeList2String(a.head);
-let numB = +reverseLinkedNodeList2String(b.head);
+// let numA = +reverseLinkedNodeList2String(a.head);
+// let numB = +reverseLinkedNodeList2String(b.head);
+//
+// let numC = numA + numB;
+// let strC = numC + '';
+//
+// let res = new LinkedNodeList();
+// for (let i = strC.length - 1; i >= 0; i--) {
+//     res.append(strC.charAt(i))
+// }
+//
+// console.log(res)
 
-let numC = numA + numB;
-let strC = numC + '';
+/**
+ * 思路
+ * 1、取出两个数的同一位（有可能一个取不到，取不到就补0）
+ * 2、将两数相加（有可能不是个位数，需要考虑前一位的进位）
+ * 3、相加得到的结果求余数得到最终结果的一位
+ * 4、相加得到的结果除10向下取整得到进位
+ * 5、设置目标链表的head和tail（考虑head不存在时）
+ * 5、改变游标，继续下一位
+ */
 
-let res = new LinkedNodeList();
-for (let i = strC.length - 1; i >= 0; i--) {
-    res.append(strC.charAt(i))
+function addTwoNumbers(l1, l2) {
+    let head, tail, carry = 0;
+    while (l1 || l2) {
+        const n1 = l1 ? l1.val : 0;
+        const n2 = l2 ? l2.val : 0;
+        const sum = n1 + n2 + carry;
+        if (!head) {
+            head = tail = new NodeList(sum % 10);
+        } else {
+            tail.next = new NodeList(sum % 10);
+            tail = tail.next;
+        }
+
+        carry = Math.floor(sum / 10);
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    if (carry > 0) tail.next = new NodeList(carry);
+    return head
 }
 
-console.log(res)
+console.log(addTwoNumbers(a.head, b.head))
+
+
+
+
+
 
 
